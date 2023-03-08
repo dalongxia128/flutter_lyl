91b56e43855b0136ee35f0e4cf3617888fddc157
刘云龙 <dalongxiabj@163.com>
2023-03-08 17:49:20 +0800

    state

--- lyl_app/lib/main.dart
+++ lyl_app/lib/main.dart
@@ -112,15 +112,69 @@ Widget titleSection = Container(
           ],
         ),
       ),
-      /*3*/
-      Icon(
-        Icons.star,
-        color: Colors.blue,
-      ),
-      const Text('43'),
+      // /*3*/
+      // Icon(
+      //   Icons.star,
+      //   color: Colors.red[500],
+      // ),
+      // const Text('43'),
+      const FavoriteWidget(),
     ],
   ),
 );
+
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({Key? key}) : super(key: key);
+
+  @override
+  State<FavoriteWidget> createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            padding: EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFavorited
+                ? const Icon(Icons.star)
+                : const Icon(
+                    Icons.star_border,
+                  )),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: SizedBox(
+            child: Text("$_favoriteCount"),
+          ),
+        )
+      ],
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+}
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
